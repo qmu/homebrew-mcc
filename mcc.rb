ffe7e96793704124e94e424869c09c3e87fc5a9f
TAMURA Yoshiya <a@qmu.jp>
2017-08-21 18:39:45 +0900

    update formula

--- mcc.rb
+++ mcc.rb
@@ -3,6 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 HOMEBREW_MCC_VERSION='0.9.2'
+CONFIG_SCHEMA_VERSION='1.0.0'
 class Mcc < Formula
   desc "A terminal dashboard, command launcher, GitHub client.. etc"
   homepage ""
@@ -18,9 +19,10 @@ class Mcc < Formula
     ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
     dir = buildpath/"src/github.com/qmu/mcc"
     dir.install Dir["*"]
+    ldflags = "-ldflags \"-X main.Version=$(#{VERSION}) -X main.ConfigSchemaVersion=$(#{CONFIG_SCHEMA_VERSION})\""
     cd dir do
       system "glide", "install"
-      system "go", "build", "-o", bin/"mcc"
+      system "go", "build", ldflags, "-o", bin/"mcc"
     end
   end
 
