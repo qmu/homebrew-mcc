6441fc3e4777ba900193c96d80265e23d577eaaa
TAMURA Yoshiya <a@qmu.jp>
2017-08-21 20:08:48 +0900

    update formula

--- mcc.rb
+++ mcc.rb
@@ -1,9 +1,9 @@
-HOMEBREW_MCC_VERSION="0.9.2".freeze
+VERSION="0.9.2".freeze
 CONFIG_SCHEMA_VERSION="1.0.0".freeze
 class Mcc < Formula
   desc "The mcc is a terminal dashboard, command launcher, GitHub client.. etc"
   homepage "https://github.com/qmu"
-  url "https://github.com/qmu/mcc.git", :tag => "v#{HOMEBREW_MCC_VERSION}"
+  url "https://github.com/qmu/mcc/releases/download/v#{VERSION}/darwin_amd64_mcc"
   sha256 "aabf03f48171c5da072e86415419fc87cfa1bd15a8601647f5ad39751131df57"
   head "https://github.com/qmu/mcc.git", :branch => "master"
 
@@ -16,9 +16,7 @@ class Mcc < Formula
     dir = buildpath/"src/github.com/qmu/mcc"
     dir.install Dir["*"]
     cd dir do
-      system "glide", "install"
-      system "go", "build", "-o", "bin/mcc", "-ldflags", "-X main.Version=#{HOMEBREW_MCC_VERSION}", "-ldflags", "-X main.ConfigSchemaVersion=#{CONFIG_SCHEMA_VERSION}"
-      bin.install "bin/mcc"
+      bin.install "mcc"
     end
   end
 
