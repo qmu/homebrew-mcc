89a769961b86b0ea7c1163914acb9b81f3eb64e5
TAMURA Yoshiya <a@qmu.jp>
2017-08-21 20:18:58 +0900

    update formula

--- mcc.rb
+++ mcc.rb
@@ -7,17 +7,8 @@ class Mcc < Formula
   sha256 "f55c69663cf027dfab93d72807351c41578aafcbbb7551a62f3d03a6fad9852f"
   head "https://github.com/qmu/mcc.git", :branch => "master"
 
-  depends_on "go" => :build
-  depends_on "glide" => :build
-
   def install
-    ENV["GOPATH"] = buildpath
-    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
-    dir = buildpath/"src/github.com/qmu/mcc"
-    dir.install Dir["*"]
-    cd dir do
-      bin.install "mcc"
-    end
+    bin.install "mcc"
   end
 
   test do
