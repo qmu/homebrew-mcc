a1e20d5ffd0700aeaa2519b10f2b7a7d803d471d
TAMURA Yoshiya <a@qmu.jp>
2017-08-21 19:37:38 +0900

    fix some problems which brew audit intends

--- mcc.rb
+++ mcc.rb
@@ -1,17 +1,13 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
-HOMEBREW_MCC_VERSION='0.9.2'
-CONFIG_SCHEMA_VERSION='1.0.0'
+HOMEBREW_MCC_VERSION="0.9.2".freeze
+CONFIG_SCHEMA_VERSION="1.0.0".freeze
 class Mcc < Formula
-  desc "A terminal dashboard, command launcher, GitHub client.. etc"
-  homepage ""
-  url 'https://github.com/qmu/mcc.git', :tag => "v#{HOMEBREW_MCC_VERSION}"
+  desc "The mcc is a terminal dashboard, command launcher, GitHub client.. etc"
+  homepage "https://github.com/qmu"
+  url "https://github.com/qmu/mcc.git", :tag => "v#{HOMEBREW_MCC_VERSION}"
   sha256 "aabf03f48171c5da072e86415419fc87cfa1bd15a8601647f5ad39751131df57"
-  head 'https://github.com/qmu/mcc.git', :branch => 'master'
+  head "https://github.com/qmu/mcc.git", :branch => "master"
 
-  depends_on 'go' => :build
+  depends_on "go" => :build
   depends_on "glide" => :build
 
   def install
